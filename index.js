45c41062d8da29f3c6008f0b845eea8b4d0cd00b
Reynolds Addy <reynoldsfrimpong@gmail.com>
2024-05-12 14:49:55 -0500

    I created modified the idexfile and created a read me

--- index.js
+++ index.js
@@ -1,54 +1,36 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const { Triangle, Circle, Square } = require("./lib/shapes.js");
-const { Font } = require("./lib/text.js");
-const { Color } = require("./lib/color.js");
+// const { Color } = require("./lib/color.js");
+// const { Font } = require("./lib/font.js");
+const SvgFile = require("./lib/SvgFile.js");
 
-
-class SvgFile{
-  constructor(){
-      this.textElement = ''
-      this.shapeElement = ''
-  }
-  render(){
-      return `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="300" height="200">${this.shapeElement}${this.textElement}</svg>`
-  }
-  setTextElement(text,color){
-      this.textElement = `<text x="150" y="125" font-size="60" text-anchor="middle" fill="${color}">${text}</text>`
-  }
-  setShapeElement(shape){
-      this.shapeElement = shape.render()
-  }
-}
-
-
-
-//  user for input using Inquirer
+// Function to prompt user for input using Inquirer
 const questions = [
-    {
-      type: "input",
-      name: "text",
-      message: "Enter up to three letters you want on your logo:",
-    },
-    {
-      type: "input",
-      name: "textColour",
-      message: "Enter the text color (hexadecimal format or color name):",
-    },
-    {
-      type: "list",
-      name: "shapeType",
-      message: "Select a shape:",
-      choices: ["triangle", "circle", "square"],
-    },
-    {
-      type: "input",
-      name: "shapeColour",
-      message: "Enter the shape color (hexadecimal format or color name):",
-    },
-  ];
+  {
+    type: "input",
+    name: "text",
+    message: "Enter up to three letters you want on your logo:",
+  },
+  {
+    type: "input",
+    name: "textColour",
+    message: "Enter the text color (hexadecimal format or color name):",
+  },
+  {
+    type: "list",
+    name: "shapeType",
+    message: "Select a shape:",
+    choices: ["triangle", "circle", "square"],
+  },
+  {
+    type: "input",
+    name: "shapeColour",
+    message: "Enter the shape color (hexadecimal format or color name):",
+  },
+];
 
-// Function to create SVG file
+// Function to initialize app
 function writeToFile(fileName, data) {
   fs.writeFile(fileName, data, (err) => {
     if (err) {
@@ -59,93 +41,54 @@ function writeToFile(fileName, data) {
   });
 }
 
-// Function to write HTML file containing SVG image
-function writeHTMLFile(fileName, svgData) {
-  const htmlContent = `
-  <!DOCTYPE html>
-  <html lang="en">
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Logo Viewer</title>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/canvg/3.0.0-alpha.3/canvg.min.js"></script>
-  </head>
-  <body>
-    <h1>Your Logo</h1>
-    <div id="logo-container">
-      ${svgData} <!-- Embed SVG content directly -->
-    </div>
-    <script src="script.js"></script>
-  </body>
-  </html>`;
 
-  // Write HTML content to file
-  fs.writeFile(fileName, htmlContent, (err) => {
-    if (err) {
-      console.log("Error creating HTML file:", err);
-    } else {
-      console.log("HTML file created successfully!");
-    }
-  });
-}
 
 // Function to initialize app
-function init() {
-  promptUser()
-    .then((answers) => {
-      let userShape;
-      // Create the appropriate shape based on user input
-      switch (answers.shapeType.toLowerCase()) {
-        case "triangle":
-          userShape = new Triangle();
-          break;
-        case "circle":
-          userShape = new Circle();
-          break;
-        case "square":
-          userShape = new Square();
-          break;
-        default:
-          throw new Error("Invalid shape type!");
-      }
-
-      const userColor = new Color(answers.shapeColour);
-
-      // Call methods to set color, text, etc.
-      userShape.setColor(userColor);
-
-      // Generate SVG data
-      const svgData = generateSVG(userShape);
-
-      // Write SVG data to SVG file
-      const svgFileName = "logo.svg";
-      writeToFile(svgFileName, svgData);
-
-      // Generate SVG data and write to HTML file
-      const htmlFileName = "logo.html";
-      writeHTMLFile(htmlFileName, svgData);
-    })
-    .catch((error) => {
-      console.log("Error:", error);
-    });
-}
-
-// Function to generate SVG from shape object
-function generateSVG(shape) {
-  if (shape instanceof Triangle) {
-    // Logic to generate SVG for a triangle
-    return `<polygon points="0,0 ${shape.base},0 ${shape.base / 2},${shape.height}" fill="${shape.color.getHex()}" />`;
-  } else if (shape instanceof Circle) {
-    // Logic to generate SVG for a circle
-    const diameter = shape.radius * 2;
-    return `<circle cx="${shape.radius}" cy="${shape.radius}" r="${shape.radius}" fill="${shape.color.getHex()}" />`;
-  } else if (shape instanceof Square) {
-    // Logic to generate SVG for a square
-    return `<rect x="0" y="0" width="${shape.side}" height="${shape.side}" fill="${shape.color.getHex()}" />`;
+async function init() {
+  let svgString = "";
+  const answers = await inquirer.prompt(questions);
+  let user_text = "";
+  if (answers.text.length > 0 && answers.text.length < 4) {
+    user_text = answers.text;
   } else {
-    throw new Error('Unsupported shape type!');
+    console.log("you need only three characters");
+    return;
   }
+  console.log("User text: [" + user_text + "]");
+
+  //user font color
+  const userFontColor = answers.textColour;
+  console.log(`User font color: ${userFontColor}`);
+
+  //user shape color
+  const userShapeColor = answers.shapeColour;
+  console.log(`User shape color:${userShapeColor}`);
+
+  //user shape type
+  let user_shape;
+  if (answers.shapeType === "Square" || answers.shapeType === "square") {
+    user_shape = new Square(userShapeColor);
+    user_shape.setColor(answers.shapeColour);
+
+    console.log("User selected Square shape");
+  } else if (answers.shapeType === "Circle" || answers.shapeType === "circle") {
+    user_shape = new Circle(userShapeColor);
+    user_shape.setColor(answers.shapeColour);
+
+    console.log("User selected Circle shape");
+  } else if (answers.shapeType === "Triangle" || answers.shapeType === "triangle") {
+    user_shape = new Triangle(userShapeColor);
+    user_shape.setColor(answers.shapeColour);
+
+    console.log("User selected Triangle shape");
+  } else {
+    console.log("Invalid shape!");
+  }
+  let svg = new SvgFile();
+  svg.setTextElement(user_text, userFontColor);
+  svg.setShapeElement(user_shape);
+  svgString = svg.render();
+  writeToFile("logo.svg", svgString); // Specify the file name here
 }
 
-// Function call to initialize app
 init();
---
+++ readme.md
@@ -0,0 +1,43 @@
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+# README GENERATOR
+
+# Table of Contents
+
+* [Introduction](#introduction)
+* [Installation](#installation)
+* [Usage](#usage)
+* [Contribution](#contribution)
+* [Tests](#tests)
+* [License](#license)
+* [Questions](#questions)
+
+## Introduction
+It is used to generate a logo.
+
+## Description
+It is ment to create a quick logo for clients who strugle in coming up with a dummy logo.
+
+## Installation
+DOWNLOAD THE APP, follow the prompt and create.
+
+## Usage
+It will be used by any persons that is now starting a business or struggling to hire a graphic Designer
+
+## Contribution
+I design the work flow of the app.
+
+## Tests
+The test passed 
+
+## License
+## License
+This application is licensed under the MIT License license. [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+
+## Questions
+For any questions about the project, feel free to reach out to me via:
+- GitHub: https://github.com/Reynoldscode/readme-generator
+
+- Screen Recording: https://drive.google.com/file/d/1wQH4XrEBtU-C6TV2SUeSwt9B2dgFmr9q/view
+
+- Email: reynoldsfrimpong@gmail.com
